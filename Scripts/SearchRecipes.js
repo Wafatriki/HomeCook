8e58e37622d45012a9ed948885880569a6f09202
Wafatriki <azdadwafa0@gmail.com>
2025-03-11 18:15:56 +0000

    refactor js

---
+++ Scripts/SearchRecipes.js
@@ -0,0 +1,17 @@
+function loadTemplate(fileName, id, callback) {
+
+    fetch(fileName).then((res) => {
+        return res.text();
+    }).then((text) => {
+        document.getElementById(id).innerHTML = text;
+        //console.log(text)
+
+        if(callback){
+            callback();
+        }
+    })
+}
+
+function loadTemplateFromSource(source, id){
+    loadTemplate(source, id);
+}
