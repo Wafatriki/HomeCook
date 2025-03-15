ad5af1566f87634245474ff690f474a4e1e9f0d1
Nicolás Rey Alonso <NicolasReyAlonso@gmail.com>
2025-03-15 17:13:14 +0000

    Add Main JSON-Server File Update HomePage.js to implement dinamic content Update HomePage.css Update index.html

--- Scripts/HomePage.js
+++ Scripts/HomePage.js
@@ -16,6 +16,25 @@ function init() {
     loadTemplate('Templates/header.html', 'header');
     loadTemplate('Templates/footer.html', 'footer');
 }
+function loadContent(){
+    var datos = fetch("http://localhost:3000/users/1")
+    .then(res => res.json())
+    .then(usuario => {
+        var RecipeOfTheDayTitle =document.getElementById('Recipe_Of_The_Day_Title');
+        console.log(usuario);
+        a.textContent= usuario.first_name;
+    })
+}
+function loadRecipeOfTheDay(){
+    var datos = fetch("http://localhost:3000/RecipeOfTheDay/")
+        .then(res => res.json())
+        .then(recipe => {
+            var RecipeOfTheDayTitle =document.getElementById('Recipe_Of_The_Day_Title');
+            var RecipeOfTheDayDescription =document.getElementById('Recipe_Of_The_Day_Text');
+            RecipeOfTheDayTitle.textContent= recipe[0].Name;
+            RecipeOfTheDayDescription.textContent= recipe[0].Description;
+        })
+}
 function load_template_from_page(template_name, id, callback) {
 
 }
