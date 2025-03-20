1d9a86e4e59d8441d43fa3c2209415e7133430a5
Nicolás Rey Alonso <NicolasReyAlonso@gmail.com>
2025-03-20 20:23:49 +0000

    ADD MainRecipe image ChocoVegano.webp UPDATE All.json UPDATE HomePage.js UPDATE index.html

--- Scripts/HomePage.js
+++ Scripts/HomePage.js
@@ -24,8 +24,12 @@ function loadRecipeOfTheDay(){
     fetch("http://localhost:3000/RecipeOfTheDay/")
         .then(res => res.json())
         .then(recipe => {
+            var RecipeOfTheDayImage = document.getElementById("Imagen_Receta_Principal");
             var RecipeOfTheDayTitle =document.getElementById('Recipe_Of_The_Day_Title');
             var RecipeOfTheDayDescription =document.getElementById('Recipe_Of_The_Day_Text');
+            console.log(recipe.Image);
+            RecipeOfTheDayImage.src = recipe[0].Image;
+            RecipeOfTheDayImage.href= "Chocolate Vegano";
             RecipeOfTheDayTitle.textContent= recipe[0].Name;
             RecipeOfTheDayDescription.textContent= recipe[0].Description;
         }).catch()
