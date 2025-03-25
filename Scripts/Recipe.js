50dce55bc41ddbd6860642d304553817aeab3a22
Nicolás Rey Alonso <NicolasReyAlonso@gmail.com>
2025-03-25 18:46:24 +0000

    UPDATE CONTENT LOAD

--- Scripts/Recipe.js
+++ Scripts/Recipe.js
@@ -25,7 +25,7 @@ function loadContent(){
             const title = document.getElementById("RecipeTitle");
             title.textContent = recipe.name;
             const image = document.getElementById("MainImage");
-            image.src = "Images/Papasarrugadas.jpg";
+            image.src = recipe.Image;
             const ingredientList = document.getElementById("ingredient-list");
             const stepList = document.getElementById("bloque_de_pasos");
             while (ingredientList.firstChild) {
