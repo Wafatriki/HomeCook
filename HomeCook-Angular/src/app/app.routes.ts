b2910f9df01ac1e75ed6948b45a5e27560625a88
Wafatriki <azdadwafa0@gmail.com>
2025-04-03 22:15:23 +0100

    added pages on angular

--- HomeCook-Angular/src/app/app.routes.ts
+++ HomeCook-Angular/src/app/app.routes.ts
@@ -2,8 +2,18 @@ import { Routes } from '@angular/router';
 import {HomePageComponent} from './Pages/home/home.component';
 import {HeaderComponent} from './header/header.component';
 import { RecipeComponent } from './Pages/recipe/recipe.component';
+import {EditProfileComponent} from './Pages/edit-profile/edit-profile.component';
+import {LoginComponent} from './Pages/login/login.component';
+import {RecipesFilterComponent} from './Pages/recipes-filter/recipes-filter.component';
+import {SearchRecipesComponent} from './Pages/search-recipes/search-recipes.component';
+import {SignInComponent} from './Pages/sign-in/sign-in.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'recipe', component: RecipeComponent },
+  { path: 'edit', component: EditProfileComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'recipes-filter', component: RecipesFilterComponent },
+  { path: 'searchRecipes', component: SearchRecipesComponent },
+  { path: 'SignIn', component: SignInComponent }
 ];
