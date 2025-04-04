c1ead4b4d77ffc8079b915ae3a464c110341028c
Wafatriki <azdadwafa0@gmail.com>
2025-04-04 17:10:01 +0100

    added pages on angular 2

---
+++ HomeCook-Angular/src/app/Pages/search-recipes/search-recipes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-recipes',
+  imports: [],
+  templateUrl: './search-recipes.component.html',
+  styleUrl: './search-recipes.component.css'
+})
+export class SearchRecipesComponent {
+
+}
