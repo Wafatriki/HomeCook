f69d332900c40d44a02287c64936ab9055d5aa71
Wafatriki <azdadwafa0@gmail.com>
2025-04-22 18:57:05 +0100

    STEPS

--- HomeCook-Angular/src/app/steps-of-recipes/steps-of-recipes.component.ts
+++ HomeCook-Angular/src/app/steps-of-recipes/steps-of-recipes.component.ts
@@ -1,12 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-steps-of-recipes',
+  standalone: true, // Indica que el componente puede funcionar por sí solo
+  imports: [CommonModule], // Importa CommonModule para permitir *ngFor y *ngIf
   templateUrl: './steps-of-recipes.component.html',
   styleUrls: ['./steps-of-recipes.component.css'],
 })
-export class StepsOfRecipesComponent {
+export class StepsOfRecipesComponent implements OnChanges {
   @Input() steps: any[] = [];
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['steps']) {
+      console.log('Pasos actualizados en el componente hijo:', this.steps);
+    }
+  }
 }
-
-
