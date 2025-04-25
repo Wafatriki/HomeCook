9453d69ff73095e259b1e786c28f8b3983923bdd
Greta <g.piccoli002@studenti.unibs.it>
2025-04-25 18:38:46 +0100

    Css Receta

--- HomeCook-Angular/src/app/Pages/recipes-filter/recipes-filter.component.ts
+++ HomeCook-Angular/src/app/Pages/recipes-filter/recipes-filter.component.ts
@@ -10,30 +10,5 @@ import {ListOfRecipesComponent} from '../../list-of-recipes/list-of-recipes.comp
   styleUrls: ['./recipes-filter.component.css']
 })
 export class RecipesFilterComponent {
-  "TiposDeReceta": [
-    {
-      "Name": "Italiana",
-      "Image": "Images/pizza.png",
-      "id": "1"
-    },
-    {
-      "Name": "Pasta",
-      "Image": "Images/boloñesa.png",
-      "id": "2"
-    },
-    {
-      "Name": "Caliente",
-      "Image": "Images/soup.png",
-      "id": "3"
-    },
-    {
-      "Name": "Dulce",
-      "Image": "Images/tiramisu.png",
-      "id": "4"
-    }
-  ];
-  constructor() {}
-
-  ngOnInit(): void {}
 
 }
