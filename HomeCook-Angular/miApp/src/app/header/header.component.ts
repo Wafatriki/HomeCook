e7c98d1e5c965f7f700e0ef683d99dd11a59c77b
Nicolás Rey Alonso <NicolasReyAlonso@gmail.com>
2025-05-06 18:32:11 +0100

    FIX header

---
+++ HomeCook-Angular/miApp/src/app/header/header.component.ts
@@ -0,0 +1,84 @@
+// src/app/components/header/header.component.ts
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import {CommonModule, NgIf} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import { AuthService } from '../Services/authentication.service'
+import {SearchComponent} from "../../../../src/app/search/search.component";
+
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+  imports: [CommonModule, FormsModule, NgIf, SearchComponent]
+})
+export class HeaderComponent implements OnInit, OnDestroy {
+  authToken: string | null = null;
+  searchQuery: string = '';
+
+  constructor(private router: Router, private authService: AuthService) { }
+
+  ngOnInit(): void {
+    /*this.authService.authToken$.subscribe(token => {
+      // Si el token es una cadena vacía, lo consideramos como null
+      this.authToken = token || null;
+    });
+
+     */
+  }
+  onSearch(): void {
+    if (this.searchQuery.trim()) {
+      this.router.navigate(['/recipes-filter'], { queryParams: { search: this.searchQuery } });
+    }
+  }
+
+
+  ngOnDestroy(): void {
+    if (typeof window !== 'undefined') {
+      window.removeEventListener('storage', this.handleStorageChange);
+    }
+  }
+
+  handleStorageChange = (event: StorageEvent) => {
+    if (event.key === 'authToken' && typeof window !== 'undefined') {
+      this.authToken = window.localStorage.getItem('authToken') || null;
+    }
+  };
+
+  goToLogin() {
+    this.router.navigate(['/login']);
+  }
+
+  goToSignIn() {
+    this.router.navigate(['/SignIn']);
+  }
+  goToAccount() {
+    this.router.navigate(['/account']);
+  }
+
+  goTo(target: string) {
+    const [path, query] = target.split('?');
+    const queryParams: Record<string, string> = {};
+
+    if (query) {
+      query.split('&').forEach(param => {
+        const [key, value] = param.split('=');
+        if (key) {
+          queryParams[decodeURIComponent(key)] = decodeURIComponent(value || '');
+        }
+      });
+    }
+
+    this.router.navigate([path], { queryParams });
+  }
+
+
+}
+function fromEntriesShim(entries: [string, any][]) {
+  const obj: any = {};
+  for (const [key, value] of entries) {
+    obj[key] = value;
+  }
+  return obj;
+}
