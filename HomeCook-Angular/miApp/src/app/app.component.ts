ecd42205a11a54a3b9a676c44d9740ec050144b0
Nicolás Rey Alonso <NicolasReyAlonso@gmail.com>
2025-05-08 18:12:07 +0100

    No funciona pero esta mejor que antes

--- HomeCook-Angular/miApp/src/app/app.component.ts
+++ HomeCook-Angular/miApp/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 import {HeaderComponent} from "./header/header.component";
+import {HomepageComponent} from "./homepage/homepage.component";
+import {FooterComponent} from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet, HeaderComponent],
+  imports: [IonApp, IonRouterOutlet, HeaderComponent, HomepageComponent, FooterComponent],
 })
 export class AppComponent {
   constructor() {}
